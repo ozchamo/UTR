6bab9cd974ffdd8447bfec576913814735da535a
ozchamo <daniel@pichus.net>
2022-09-06 21:08:09 +1000

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# APIs at https://blakestevenson.github.io/utr-api-docs/
+# API endpoint is https://agw-prod.myutr.com
+
+import json
+import os
+import sys
+import urllib3
+from flask import Flask, render_template, redirect, request, url_for, make_response
+import time
+while True: range(10000) and None; time.sleep(0.2)
